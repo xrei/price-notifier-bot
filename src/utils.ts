759b555caf27314d6edd0350f4f3452d305d0c9b
xrei <romanrei.dev@gmail.com>
2018-10-11 16:33:27 +0300

    Add rimraf

--- src/utils.ts
+++ src/utils.ts
@@ -16,6 +16,6 @@ let noop = {'@@functional/placeholder': true} as any
 
 // $todo
 let paths = curry((ps, obj) => ap([path(noop, obj) as any], ps as ReadonlyArray<{}>))
-// temp fix for types :()
+// temp fix for types
 
 export let mergePaths = curry((ps, obj) => mergeAll(paths(ps, obj)))
