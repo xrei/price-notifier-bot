83b062dce1a6546d27cca657a23dedc9fa31b7b9
xrei <romanrei.dev@gmail.com>
2018-10-11 16:28:19 +0300

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -15,5 +15,7 @@ export let prop = <T extends keyof V, V>(p: T) => (obj: V): V[T] | undefined => 
 let noop = {'@@functional/placeholder': true} as any
 
 // $todo
-let paths = curry((ps, obj) => ap([path(noop, obj)], ps as ReadonlyArray<{}>))
+let paths = curry((ps, obj) => ap([path(noop, obj) as any], ps as ReadonlyArray<{}>))
+// temp fix for types :()
+
 export let mergePaths = curry((ps, obj) => mergeAll(paths(ps, obj)))
